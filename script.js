add6b3227381beb9cce6131ba432c25eee3b9c4a
snegha032002 <sneghakumar0315@gmail.com>
2024-12-30 17:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ if (i % 2 === 0 && i < 6) {
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
